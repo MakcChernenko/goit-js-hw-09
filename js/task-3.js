96473688ea751feb27fc2a1a4574a43b7add824a
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-05-23 11:07:02 +0300

    added murcap

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector('#name-input');
-const spanEl = document.querySelector('#name-output');
-inputEl.addEventListener('input', () => {
-  if (inputEl.value.trim()) {
-    spanEl.textContent = inputEl.value;
-  } else {
-    spanEl.textContent = 'Anonymous';
-  }
-});
